438262ea598c21b6f7d782bce23ab86e50384591
TestOodoO <prakashsankark@gmail.com>
2016-05-20 13:41:45 +0530

    start

--- app/views/support/mailSupport/mail1.blade.php
+++ app/views/support/mailSupport/mail1.blade.php
@@ -24,11 +24,11 @@
                   <input id="mailboxCheckbox1" type="checkbox" value="value1">
                   <label for="mailboxCheckbox1"></label>
                 </div><a href="javascript:;">
-                  <div class="media-left avatar"><img src="build/images/users/02.jpg" alt="" class="media-object img-circle"><span class="status bg-success"></span></div>
+                  <div class="media-left avatar"><img src="/assets/dist/support/images/avatar/48.jpg" alt="" class="media-object img-circle"><span class="status bg-success"></span></div>
                   <div class="media-body">
                     <h6 class="media-heading">{{$mail->from_mail}}</h6>
                     <h5 class="title">{{$mail->subject}}</h5>
-                    <p class="summary">{{$mail->body}}</p>
+                    <p class="summary">{{ Str::limit($mail->body, 150)}}</p>
                   </div>
                   <div class="media-right text-nowrap">
                     <time datetime="{{$mail->time}}" class="fs-11" data-livestamp="{{$mail->time}}"></time>
